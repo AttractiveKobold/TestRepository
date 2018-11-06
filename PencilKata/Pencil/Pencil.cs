2ffc06a6a18dc1facd278ccaf8cbae7c30e63794
Jacob Mangia <tucker2134@gmail.com>
2018-11-06 16:37:56 -0500

    a

---
+++ PencilKata/Pencil/Pencil.cs
@@ -0,0 +1,172 @@
+﻿namespace PencilLibrary
+{
+    public class Pencil
+    {
+
+        private int pointDurability = 0;
+        private int maxDurability = 0;
+        private int length = 0;
+        private int eraserDurability = 0;
+
+        private const int capitalLetterPointDegredation = 2;
+        private const int lowercaseLetterPointDegredation = 1;
+        private const int eraserDegredationAmount = 1;
+
+        public int getPointDurability() => pointDurability;
+        public int getMaxDurability() => maxDurability;
+        public int getLength() => length;
+        public int getEraserDurability() => eraserDurability;
+
+
+
+        public Pencil(int pointDurability, int length, int eraserDurability)
+        {
+            this.pointDurability = pointDurability;
+            this.maxDurability = pointDurability;
+            this.length = length;
+            this.eraserDurability = eraserDurability;
+        }
+
+
+
+        public string Write(string textToWrite, string startingString = "")
+        {
+            string output = startingString;
+
+            foreach (char c in textToWrite)
+            {
+
+                if (degradePoint(c))
+                    output += c;
+                else
+                    output += ' ';
+            }
+
+            return output;
+        }
+
+        private bool degradePoint(char myChar)
+        {
+            if (char.IsUpper(myChar) && pointDurability >= capitalLetterPointDegredation)
+            {
+                pointDurability -= capitalLetterPointDegredation;
+
+            }
+            else if (checkIfShouldDegradeLower(myChar))
+            {
+                pointDurability -= lowercaseLetterPointDegredation;
+            }
+            else
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        private bool checkIfShouldDegradeLower(char myChar)
+        {
+            if (!char.IsUpper(myChar) && !(myChar == ' ' || myChar == '\n') && pointDurability >= lowercaseLetterPointDegredation)
+                return true;
+            else
+                return false;
+        }
+
+
+
+        public string Edit(string textToWrite, string startingString, int blankSpaceToEdit = 1)
+        {
+            int index = getIndexOfBlankSpace(blankSpaceToEdit, startingString);
+
+            if (index == -1)
+                return startingString;
+
+            char[] textToEditArray = startingString.ToCharArray();
+            char[] textToWriteArray = textToWrite.ToCharArray();
+
+            for (int i = index, j = 0; i < (index + textToWriteArray.Length) && i < startingString.Length - 1; i++, j++)
+            {
+                if (degradePoint(textToWriteArray[j]) || textToWriteArray[j] == '\n')
+                {
+                    if (textToEditArray[i] == ' ')
+                        textToEditArray[i] = textToWriteArray[j];
+                    else
+                        textToEditArray[i] = '@';
+                }
+
+            }
+
+            return new string(textToEditArray);
+        }
+
+        //returns -1 if the specified blank space does not exist
+        private int getIndexOfBlankSpace(int blankSpaceToEdit, string text)
+        {
+            int index = 0;
+
+            index = text.IndexOf("  ");
+            blankSpaceToEdit--;
+
+            while (blankSpaceToEdit > 0)
+            {
+                for (int i = index; i < text.Length - 1; i++)
+                {
+                    if (index == -1)
+                        return index; //blank space was not found
+
+                    if (!char.IsWhiteSpace(text[i]))
+                    {
+                        index = i;
+                        break;
+                    }
+                }
+
+                index = text.IndexOf("  ", index);
+                blankSpaceToEdit--;
+            }
+
+            if (index > 0)
+                index++;
+
+            return index;
+        }
+
+
+
+        public void Sharpen()
+        {
+            if (length > 0)
+            {
+                pointDurability = maxDurability;
+                length--;
+            }
+        }
+
+
+
+        public string Erase(string textToErase, string startingString)
+        {
+            if (!startingString.Contains(textToErase))
+                return startingString;
+
+            string erasedText = "";
+            int indexOfTextToErase = startingString.LastIndexOf(textToErase);
+            char[] textToEraseArray = textToErase.ToCharArray();
+
+            for (int i = textToEraseArray.Length - 1; i > -1; i--)
+            {
+                if (eraserDurability < eraserDegredationAmount)
+                    break;
+
+                if (textToEraseArray[i] != ' ')
+                    eraserDurability -= eraserDegredationAmount;
+
+                textToEraseArray[i] = ' ';
+            }
+
+            erasedText = new string(textToEraseArray);
+
+            return startingString.Remove(indexOfTextToErase, textToErase.Length).Insert(indexOfTextToErase, erasedText);
+        }       
+    }
+}
---
+++ PencilKata/PencilKata/PencilTests.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using NUnitLite;
+using PencilLibrary;
+
+namespace PencilKata
+{
+    [TestFixture]
+    class PencilTests
+    {
+        Pencil pencil;
+
+        static void Main(string[] args)
+        {
+            new AutoRun().Execute(args);
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+
+        [SetUp]
+        public void testInit()
+        {
+            pencil = new Pencil(100, 100, 100);
+        }
+
+        [Test]
+        public void whenPencilIsPassedTwoStringsItAppendsTheFirstStringOntoTheSecondString()
+        {
+            Assert.AreEqual("aaaSome Test Words", pencil.Write("Some Test Words", "aaa"));
+        }
+
+        [Test]
+        public void whenALowercaseLetterIsWrittenDurabilityDegradesByOne()
+        {
+
+            pencil.Write("  a b \n ");
+
+            Assert.AreEqual(98, pencil.getPointDurability());
+
+            pencil.Write("  c   ");
+
+            Assert.AreEqual(97, pencil.getPointDurability());
+        }
+
+        [Test]
+        public void whenAnUppercaseLetterIsWrittenDurabilityDegradesByTwo()
+        {
+
+            pencil.Write("A \nB");
+
+            Assert.AreEqual(96, pencil.getPointDurability());
+
+            pencil.Write(" C   ");
+
+            Assert.AreEqual(94, pencil.getPointDurability());
+
+        }
+
+        [Test]
+        public void whenSharpenedDurabilityIsReset()
+        {
+            pencil.Write("fubdfniebibnasdA");
+
+            pencil.Sharpen();
+
+            Assert.AreEqual(100, pencil.getMaxDurability());
+            Assert.AreEqual(100, pencil.getPointDurability());
+        }
+
+        [Test]
+        public void whenThereIsNotEnoughDurabilityABlankIsWritten()
+        {
+            pencil = new Pencil(5, 0, 0);
+
+            Assert.AreEqual("abcd f  ", pencil.Write("abcdEfgh"));
+
+
+        }
+
+        [Test]
+        public void whenPencilIsSharpenedLengthIsReducedByOne()
+        {
+            pencil = new Pencil(5, 1, 0);
+
+            pencil.Write("aaaaa");
+            pencil.Sharpen();
+
+            Assert.AreEqual(0, pencil.getLength());
+
+        }
+
+        [Test]
+        public void whenPencilLengthIsZeroPencilCannotBeSharpened()
+        {
+            pencil = new Pencil(5, 0, 0);
+
+            pencil.Write("aaaaa");
+            pencil.Sharpen();
+
+            Assert.AreEqual("     ", pencil.Write("aaaaa"));
+
+        }
+
+        [Test]
+        public void whenGivenTwoStringsReplaceTheLastInstanceOfTheFirstStringInTheSecondStringWithBlankSpaces()
+        {
+            Assert.AreEqual("Erase last instance of word          in this string", pencil.Erase("instance", "Erase last instance of word instance in this string"));
+        }
+
+        [Test]
+        public void whenGivenAStringToEraseThatIsNotInTheStartingStringReturnTheStartingString()
+        {
+            Assert.AreEqual("Nothing should erase", pencil.Erase("bacon", "Nothing should erase"));
+        }
+
+        [Test]
+        public void whenEraserIsUsedDegradeEraserDurabilityByOnePerCharacter()
+        {
+            pencil = new Pencil(5, 1, 10);
+
+            pencil.Erase(" Durability", "Reduce Durability");
+
+            Assert.AreEqual(0, pencil.getEraserDurability());
+        }
+
+        [Test]
+        public void whenEraserDurabilityIsZeroErasingStops()
+        {
+            pencil = new Pencil(5, 1, 8);
+
+            Assert.AreEqual("Reduce Du        ", pencil.Erase(" Durability", "Reduce Durability"));
+        }
+
+        [Test]
+        public void whenGivenAStringEditWritesThatStringInTheFirstBlankSpace()
+        {
+            Assert.AreEqual("Edit Test Complete", pencil.Edit("Test", "Edit      Complete"));
+        }
+
+        [Test]
+        public void whenEditingASpaceWithAStringThatIsTooLongUseTheAtSymbolInstead()
+        {
+            Assert.AreEqual("This is no@ight", pencil.Edit("not", "This is   right"));
+        }
+
+        [Test]
+        public void whenNonLetterCharactersAreWrittenPointDurabilityDegradesByOne()
+        {
+            pencil.Write("!1,./'-_=0");
+            Assert.AreEqual(90, pencil.getPointDurability());
+        }
+
+        [Test]
+        public void whenEditingAPaperPointStillDegrades()
+        {
+            pencil.Edit("Test", "Edit      Complete");
+            Assert.AreEqual(95, pencil.getPointDurability());
+        }
+
+        [Test]
+        public void whenPointDurabilityIsAtZeroEditingStops()
+        {
+            pencil = new Pencil(3, 0, 0);
+
+            Assert.AreEqual("Edit Te   Complete", pencil.Edit("Test", "Edit      Complete"));
+        }
+
+        [Test]
+        public void whenPassedIntegerNEditWillEditTheNthBlankSpace()
+        {
+            Assert.AreEqual("Edit      Complete Test Again", pencil.Edit("Test", "Edit      Complete      Again", 2));
+        }
+
+        [Test]
+        public void whenPassedABlankSpaceThatDoesNotExistEditWillNotEditAnything()
+        {
+            Assert.AreEqual("This     Should     Not     Change", pencil.Edit("aaa", "This     Should     Not     Change", 20));
+        }
+
+        [Test]
+        public void whenGivenAStringThatStartsWithABlankSpaceEditWillNotLeaveABlankAtTheBeginningOfTheString()
+        {
+            Assert.AreEqual("False: this string begins with a blank space", pencil.Edit("False:", "       this string begins with a blank space"));
+        }
+
+        [Test]
+        public void whenEditingPointDegradesBasedOnWhatIsWritten()
+        {
+            pencil.Edit("A b C d", "Test        String");
+
+            Assert.AreEqual(94, pencil.getPointDurability());
+        }
+    }
+}
